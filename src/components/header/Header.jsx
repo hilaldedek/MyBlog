1c21afb6cb466563f3bb0e5db03bffce81065ec7
hilal dedek <hdedek03@gmail.com>
2023-08-15 18:48:49 +0300

    Education section started

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,8 @@ import recipe from "../../assets/recipe.png";
 import disney from "../../assets/disney.png";
 import weather from "../../assets/weather.png";
 import todo from "../../assets/todo.png";
-import movieApp from "../../assets/movieapp.png"
+import movieApp from "../../assets/movieapp.png";
+import shopping from "../../assets/shoppig.png";
 import me from "../../assets/me3.jpg";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -165,16 +166,70 @@ const Header = () => {
        </Box> 
       </Box>
 
+{/* EDUCATİON */}
+<Box className="card" sx={{position:"relative",margin:"auto"}}>
+        <Typography  sx={{fontFamily:"Fredericka the Great",fontSize:"3.7rem",paddingTop:"2rem",textAlign:"center",color:"#DDFFBB"}}>EDUCATION</Typography>
+        <Typography sx={{fontFamily:"Fredericka the Great",fontSize:"3.7rem",transform:"rotateX(180deg)",marginTop:"-3rem",textAlign:"center",color:"#ddffbb31"}}>EDUCATION</Typography>
+<Box sx={{display:"flex",flexWrap:"wrap",justifyContent:"center"}}>
+<Card sx={{minWidth: 275, width:"25%",padding:"1rem",margin:"1.5rem 1rem"}} className='edu'>
+  <Box sx={{margin:"1rem 1rem",backgroundColor:"#000000b3",color:"#DDFFBB"}}>
+<CardContent>
+        <Typography variant="h4" component="div">
+          Trakya University
+        </Typography>
+        <Typography variant="body2" sx={{fontSize:"1.3rem"}}>
+          Computer Engineering
+        </Typography>
+        <Typography variant="p" component="div">
+          2021-2025
+        </Typography>
+      </CardContent>
+  </Box>
+    </Card>
+
+    <Card sx={{ minWidth: 275, width:"25%",padding:"1rem",margin:"1.5rem 1rem"}} className='edu'>
+      <Box sx={{margin:"1rem 1rem",backgroundColor:"#000000b3",color:"#DDFFBB"}}>
+        <CardContent>
+        <Typography variant="h4" component="div">
+          Clarusway
+        </Typography>
+        <Typography variant="body2" sx={{fontSize:"1.3rem"}}>
+          FullStack Bootcamp
+        </Typography>
+        <Typography variant="p" component="div">
+          2022-2023
+        </Typography>
+      </CardContent>
+      </Box>
+      
+    </Card>
+
+    <Card sx={{minWidth: 275, width:"25%",padding:"1rem",margin:"1.5rem 1rem"}} className='edu'>
+      <Box sx={{margin:"1rem 1rem",backgroundColor:"#000000b3",color:"#DDFFBB"}}>
+<CardContent>
+        <Typography variant="h4" component="div">
+        Lutfi Ege Anatolian High School
+        </Typography>
+        <Typography variant="p" component="div">
+          2017-2021
+        </Typography>
+      </CardContent>
+      </Box>
+      
+    </Card>
+</Box>
+</Box>
+
       {/* SKILLS SECTİON */}
       <Box className="card" sx={{padding:"1rem"}}>
         <Box sx={{display:"flex",flexWrap:"wrap",justifyContent:"center"}} id="skills"> 
       <Box sx={{marginBottom:"-4rem"}}>
-        <Typography  sx={{fontFamily:"Fredericka the Great",fontSize:"4.1rem",paddingTop:"2rem",textAlign:"center",color:"#DDFFBB"}}>TECHNICAL</Typography>
-        <Typography sx={{fontFamily:"Fredericka the Great",fontSize:"4.1rem",transform:"rotateX(180deg)",marginTop:"-3rem",textAlign:"center",color:"#ddffbb31"}}>TECHNICAL</Typography>
+        <Typography  sx={{fontFamily:"Fredericka the Great",fontSize:"3.7rem",paddingTop:"2rem",textAlign:"center",color:"#DDFFBB"}}>TECHNICAL</Typography>
+        <Typography sx={{fontFamily:"Fredericka the Great",fontSize:"3.7rem",transform:"rotateX(180deg)",marginTop:"-3rem",textAlign:"center",color:"#ddffbb31"}}>TECHNICAL</Typography>
       </Box>
       <Box sx={{marginLeft:"1rem"}}>
-        <Typography  sx={{fontFamily:"Fredericka the Great",fontSize:"4.1rem",paddingTop:"2rem",textAlign:"center",color:"#DDFFBB"}}>SKILLS</Typography>
-        <Typography sx={{fontFamily:"Fredericka the Great",fontSize:"4.1rem",transform:"rotateX(180deg)",marginTop:"-3rem",textAlign:"center",color:"#ddffbb31"}}>SKILLS</Typography>
+        <Typography  sx={{fontFamily:"Fredericka the Great",fontSize:"3.7rem",paddingTop:"2rem",textAlign:"center",color:"#DDFFBB"}}>SKILLS</Typography>
+        <Typography sx={{fontFamily:"Fredericka the Great",fontSize:"3.7rem",transform:"rotateX(180deg)",marginTop:"-3rem",textAlign:"center",color:"#ddffbb31"}}>SKILLS</Typography>
       </Box>
     </Box>
     <Box>
@@ -216,6 +271,24 @@ const Header = () => {
                         Bootstrap
               </Button>
               <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",padding:"1rem",color:"white",margin:"1rem"}} className='skillsButton'>
+                        Django
+              </Button>
+              <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",padding:"1rem",color:"white",margin:"1rem"}} className='skillsButton'>
+                        MongoDB
+              </Button>
+              <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",padding:"1rem",color:"white",margin:"1rem"}} className='skillsButton'>
+                        Python
+              </Button>
+              <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",padding:"1rem",color:"white",margin:"1rem"}} className='skillsButton'>
+                        MYSQL
+              </Button>
+              <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",padding:"1rem",color:"white",margin:"1rem"}} className='skillsButton'>
+                        SQLITE
+              </Button>
+              <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",padding:"1rem",color:"white",margin:"1rem"}} className='skillsButton'>
+                        Docker
+              </Button>
+              <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",padding:"1rem",color:"white",margin:"1rem"}} className='skillsButton'>
                         Git
               </Button>
               <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",padding:"1rem",color:"white",margin:"1rem"}} className='skillsButton'>
@@ -241,7 +314,7 @@ const Header = () => {
         </Typography>
         <Typography sx={{fontFamily:"Fredericka the Great",fontSize:"4.1rem",transform:"rotateX(180deg)",marginTop:"-3rem",textAlign:"center",color:"#ddffbb31"}}>PROJECTS</Typography>
         <Box sx={{display:"flex",flexWrap:"wrap",justifyContent:"center",margin:"2rem"}} >
-          
+          {/* RECİPE APP */}
           <Box className="cardGif" sx={{padding:"1rem",margin:"1.5rem 1rem",}}>
             <Card sx={{ maxWidth: 345,margin:"1rem 1rem",backgroundColor:"#000000b3"}}>
               <CardActionArea sx={{padding:"1rem"}}>
@@ -264,7 +337,7 @@ const Header = () => {
               </CardActions>
             </Card>
           </Box>
-              
+              {/* WEATHER APP */}
           <Box className="cardGif" sx={{padding:"1rem",margin:"1.5rem 1rem"}}>
             <Card sx={{ maxWidth: 345,margin:"1rem",backgroundColor:"#000000b3" }}>
               <CardActionArea sx={{padding:"1rem"}}>
@@ -287,7 +360,7 @@ const Header = () => {
               </CardActions>
             </Card>
           </Box>
-            
+            {/* TO DO APP */}
             <Box className="cardGif" sx={{padding:"1rem",margin:"1.5rem 1rem"}}>
               <Card sx={{ maxWidth: 345,margin:"1rem 1rem",backgroundColor:"#000000b3" }}>
               <CardActionArea sx={{padding:"1rem"}}>
@@ -311,7 +384,7 @@ const Header = () => {
               </CardActions>
             </Card>
             </Box>
-            
+            {/* DİSNEY  */}
             <Box className="cardGif" sx={{padding:"1rem",margin:"1.5rem 1rem"}}>
 <Card sx={{ maxWidth: 345,margin:"1rem 1rem",backgroundColor:"#000000b3" }}>
               <CardActionArea sx={{padding:"1rem"}}>
@@ -335,6 +408,7 @@ const Header = () => {
               </CardActions>
             </Card>
             </Box>
+            {/* MOVİE APP */}
             <Box className="cardGif" sx={{padding:"1rem",margin:"1.5rem 1rem",}}>
             <Card sx={{ maxWidth: 345,margin:"1rem 1rem",backgroundColor:"#000000b3"}}>
               <CardActionArea sx={{padding:"1rem"}}>
@@ -357,14 +431,38 @@ const Header = () => {
               </CardActions>
             </Card>
           </Box>
-            
+            {/* SHOPPİNG PAGE */}
+            <Box className="cardGif" sx={{padding:"1rem",margin:"1.5rem 1rem"}}>
+<Card sx={{ maxWidth: 345,margin:"1rem 1rem",backgroundColor:"#000000b3" }}>
+              <CardActionArea sx={{padding:"1rem"}}>
+                <CardMedia
+                  component="img"
+                  height="170"
+                  image={shopping}
+                  alt="shoppingPageProject"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div" sx={{fontFamily: 'Julius Sans One',textAlign:"center",fontWeight:"900",fontSize:"2rem",color:"#DDFFBB"}}>
+                    Shopping Page
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",position:"relative",margin: "auto",color:"#393053",backgroundColor:"#d8cbff90",marginBottom:"1.5rem"}}>
+                <Link href='https://hilaldedek.github.io/Javascript-ShoppingPage/' target='__blank' underline='none' sx={{color:"white"}}>Live</Link>
+                </Button>
+              </CardActions>
+            </Card>
+            </Box>
         </Box>
+        {/* MORE PROJECTS SECTİON */}
         <Box sx={{display:"flex",justifyContent:"center",marginTop:"2rem"}}>
           <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",padding:"1rem",}} >
                     <Link href='https://github.com/hilaldedek?tab=repositories' target='__blank' underline='none' sx={{color:"#F6F1E9"}}>More Project</Link>
           </Button>
         </Box>
       </Box>
+
      {/* CONTACT SECTION */}
      <Box sx={{paddingTop:"4rem",paddingBottom:"4rem"}} className="card" id="contact">
      <Box sx={{display:"flex",flexWrap:"wrap",justifyContent:"center"}}> 
@@ -378,6 +476,7 @@ const Header = () => {
       </Box>
     </Box>
         <Box sx={{display:"flex",flexWrap:"wrap",justifyContent:"center",marginTop:"1rem"}}>
+          {/* LİNKEDİN */}
           <Box sx={{margin:"1rem",padding:"1rem",borderRadius:"100px"}} className="contactGif">
             <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",position:"relative",margin: "auto",color:"white",padding:"1rem"}}>
                 <Link href='https://www.linkedin.com/in/hil%C3%A2ldedek/' target='__blank' sx={{color:"#ffffffde"}}><LinkedInIcon
@@ -385,16 +484,19 @@ const Header = () => {
             /></Link>
             </Button>
           </Box>
+          {/* GİTHUB */}
           <Box sx={{margin:"1rem",padding:"1rem",borderRadius:"100px"}} className="contactGif">
             <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",position:"relative",margin: "auto",padding:"1rem"}} >
                 <Link href='https://github.com/hilaldedek' target='__blank' sx={{color:"#ffffffde"}} ><GitHubIcon sx={{width:"4rem",height:"4rem"}}/></Link>
             </Button>
           </Box>  
+          {/* INSTAGRAM */}
           <Box sx={{margin:"1rem",padding:"1rem",borderRadius:"100px"}} className="contactGif">
             <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",position:"relative",margin: "auto",color:"white",padding:"1rem"}}>
                 <Link href='https://www.instagram.com/hilaldedek/' target='__blank' underline='none' sx={{color:"#ffffffde"}}><InstagramIcon sx={{width:"4rem",height:"4rem"}}/></Link>
             </Button>
           </Box> 
+          {/* TWİTTER */}
           <Box sx={{margin:"1rem",padding:"1rem",borderRadius:"100px"}} className="contactGif">
             <Button sx={{fontFamily:"Julius Sans One",fontWeight:"900",fontSize:"1rem",position:"relative",margin: "auto",color:"white",padding:"1rem"}}>
                 <Link href='https://twitter.com/Hilitomilito1' target='__blank' underline='none' sx={{color:"#ffffffde"}}><TwitterIcon sx={{width:"4rem",height:"4rem"}}/></Link>
